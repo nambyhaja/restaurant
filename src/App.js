77002446fe9467d3829a0bc07aec25bab4956c8d
Razafindrafito Nambinintsoa Haja <nambinintsoa.razafindrafito@gmail.com>
2019-04-09 23:11:38 +0300

    test accueil in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Button from '@material-ui/core/Button';
 import './App.css';
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { Route, Link, BrowserRouter as Router, Switch  } from 'react-router-dom'
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-import Ajout from './Admin/Ajout';
 import Login from './Admin/Login';
 import Notfound from './Error/NotFound';
 import Restaurant from './Components/Restaurant';
+import Accueil from './Accueil';
 
 
 // If you want your app to work offline and load faster, you can change
@@ -28,10 +28,13 @@ const routing = (
             <li>
             <Link to="/admin/login">Login</Link>
             </li>
+            <li>
+            <Link to="/accueil">Accueil</Link>
+            </li>
         </ul>
         <Switch>
             <Route exact path="/" component={App} />
-            <Route path="/admin/ajout" component={Ajout} />
+            <Route path="/accueil" component={Accueil} />
             <Route path="/admin/login" component={Login} />
             <Route path="/restaurant/:id" component={Restaurant} />
             <Route component={Notfound} />
